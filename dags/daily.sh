38ba413ff2ca616deb2ea5561bd121a163be0cc8
bbbales2 <bbbales2@gmail.com>
2022-09-17 15:03:46 -0700

    Updated daily script

--- dags/daily.sh
+++ dags/daily.sh
@@ -1,9 +1,10 @@
+#!/bin/bash
 # Run this from the same directory that contains the Dockerfile
 git pull
 docker build -t chuckwalla2 .
 docker run \
-  -e AWS_ACCESS_KEY_ID=$(aws configure get default.aws_access_key_id) \
-  -e AWS_SECRET_ACCESS_KEY=$(aws configure get default.aws_secret_access_key) \
-  -e AWS_DEFAULT_REGION=$(aws configure get default.region) \
+  -e AWS_ACCESS_KEY_ID="$(aws configure get default.aws_access_key_id)" \
+  -e AWS_SECRET_ACCESS_KEY="$(aws configure get default.aws_secret_access_key)" \
+  -e AWS_DEFAULT_REGION="$(aws configure get default.region)" \
   chuckwalla2 \
-  --time `date -u +%F`
+  --time "$(date -u +%F)"
